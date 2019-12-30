0f877c569a9374ca56f4df9ce7bea3531cc98f46
Kenta Kase <kesin1202000@gmail.com>
2019-12-30 11:16:57 +0900

    dotnet new

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = args[0];
+        Person person = new Person("john", name);
+        Console.WriteLine(person);
+        person.DisplayName();
+    }
+}
+
+public class Person
+{
+    private string fname;
+    private string lname;
+    public Person(string firstName, string lastName)
+    {
+        fname = firstName;
+        lname = lastName;
+    }
+
+    public override string ToString() => $"{fname} {lname}".Trim();
+    public void DisplayName() => Console.WriteLine(ToString());
+}
