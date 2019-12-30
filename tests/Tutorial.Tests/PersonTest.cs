05c5765c726372ed59aa4aa6d5fa8e0125636b01
Kenta Kase <kesin1202000@gmail.com>
2019-12-30 23:51:36 +0900

    Add test

--- tests/Tutorial.Tests/PersonTest.cs
+++ tests/Tutorial.Tests/PersonTest.cs
@@ -5,9 +5,25 @@ namespace Tutorial.Tests
 {
     public class PersonTest
     {
+        int i;
+
+        public PersonTest()
+        {
+            i = 1;
+        }
+
         [Fact]
         public void Test1()
         {
+            Assert.Equal(1, i);
+
+        }
+
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(2, i);
 
         }
     }
